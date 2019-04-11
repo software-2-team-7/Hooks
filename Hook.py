5f7aef5ef1743e90b986642182dc1f3c6d39c0e8
bcalvarado <bcalvarado@miners.utep.edu>
2019-04-11 14:53:29 -0600

    Update Hook.py

--- Hook.py
+++ Hook.py
@@ -1,10 +1,10 @@
 def   increment_association_number():
-  print("Hello from a function")
+  print("Hello from increment_association_number")
 
 def   decrement_association_number():
-  print("Hello from a function")
+  print("Hello from decrement_association_number")
 
 def   execute():
-  print("Hello from a function")
+  print("Hello from execute")
 
 decrement_association_number()
