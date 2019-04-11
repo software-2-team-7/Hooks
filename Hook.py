d8f287a34544dcdba464c6bf8be115553ddd771f
Bianca Chanel Alvarado <biancachanelalvarado@Biancas-MacBook-Pro-3.local>
2019-04-11 14:49:53 -0600

    Added test methods

--- Hook.py
+++ Hook.py
@@ -1,1 +1,10 @@
+def   increment_association_number():
+  print("Hello from a function")
 
+def   decrement_association_number():
+  print("Hello from a function")
+
+def   execute():
+  print("Hello from a function")
+
+decrement_association_number()
