d4776510c2b0258de0bd11a5ab7bf1bd3862a000
Bianca Chanel Alvarado <biancachanelalvarado@Biancas-MacBook-Pro-3.local>
2019-04-11 17:24:46 -0600

    Added more functions and attributes for Hook and Hook Collection

--- Hook.py
+++ Hook.py
@@ -16,3 +16,19 @@ def execute():
 
 def know_hook_attributes():
     print("Hello from a function") 
+
+def execute_hook_sequence():
+    print("Hello from a function")
+
+def __add_hook(): #private
+    print("Hello") 
+
+def __update_hook_sequencing(): #private
+    print("Hello")
+
+def __update_hook_collection_sequencing(): #private
+    print("Hello")
+
+def __remove_hook(): #private
+    print("Hello")
+
--- HookCollection.py
+++ HookCollection.py
@@ -1,1 +1,30 @@
+class HookCollection:
+    Hook_Collection_Name = ""
+    Hook_Collection_Sequence_Number = 0
+    Hook_Collection_Status = False
+    Hook_Collection_Description = ""
+    Hooks = [] #Hooks: List<Hook> 
 
+def knowIfHookSequenceIsUnique():
+    print("A functio")
+def knowIfHookCollectionSequenceIsUnique():
+    print("A function")
+def searchHooks():
+    print("A function")
+def deleteHook():
+    print("A function")
+
+def execute_hook_sequence():
+    print("Hello from a function")
+
+def __add_hook(): #private
+    print("Hello") 
+
+def __update_hook_sequencing(): #private
+    print("Hello")
+
+def __update_hook_collection_sequencing(): #private
+    print("Hello")
+
+def __remove_hook(): #private
+    print("Hello")
