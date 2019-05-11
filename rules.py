0ae308df2d2d09368e7a3125500cadabc03dff78
root <root@localhost.localdomain>
2019-05-10 22:22:17 -0600

    add rules class and implemented rules into int2

---
+++ rules.py
@@ -0,0 +1,19 @@
+from netfilterqueue import *
+import socket, os
+
+class rules(object):
+
+	def __init__(self):
+		print("i am iptables")
+
+	def flush(self):
+		print("\niptables flushed")
+		os.system("iptables --flush")
+
+	def set(self):
+		print("iptables set")
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(("8.8.8.8", 80))
+		ip_addr = s.getsockname()[0]
+		os.system("iptables -I INPUT -d "+ip_addr+" -j NFQUEUE --queue-num 1")
+
