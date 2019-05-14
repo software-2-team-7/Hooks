6f36b494d2d09fd15e2b142b21d879294bc7672b
Bianca Chanel Alvarado <biancachanelalvarado@Biancas-MacBook-Pro-3.local>
2019-05-13 22:30:07 -0600

    Hook Collection Seq Num Sorting

--- HookManager/HookCollectionManager.py
+++ HookManager/HookCollectionManager.py
@@ -14,5 +14,16 @@ class HookCollectionManager(object):
     def executeCollection(self,packet):
         for h in self.collection:
             h.executeHookSequence(packet)
+
+    def insertionSort_Hook_Collection_Sequence_Number(self):
+        for index in range(1,len(self.collection)):
+            currentHookCollection = self.collection[index]
+            currentvalue = self.collection[index].Hook_Collection_Sequence_Number
+            position = index
+            while position>0 and self.collection[position-1].Hook_Collection_Sequence_Number > currentvalue:
+                self.collection[position]=self.collection[position-1]
+                position = position-1
+
+            self.collection[position]=currentHookCollection
     
 
