d815d56d0930c265b1f635deeda2e8b877a9c8cd
Bianca Chanel Alvarado <biancachanelalvarado@Biancas-MacBook-Pro-3.local>
2019-05-16 01:11:20 -0600

    Added to file

--- HookManager/handshake.py
+++ HookManager/handshake.py
@@ -1,8 +1,16 @@
 from kamene.all import *
+##from Scapy.all import *
 
 class run:
     def execute(self, packet):
-        dst = packet.dst
-        src = packet.src
-        srcPort = packet.sport
-        dstPort = packet.dport
+        if(packet.haslayer(TCP)):
+            dst = packet.dst
+            src = packet.src
+            srcPort = packet.sport
+            dstPort = packet.dport
+            ack = packet.ack + 1 
+            seq = packet.seq
+            ip = IP(srrc = src, dst = dst)
+            SYN = TCP(sport = srcPort, dport = dstPort, seq = seq, ack = ack)
+            SYN_ACK = sr1(ip/SYN)
+
