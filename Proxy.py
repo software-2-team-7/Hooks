28b0f4ccfacf50c4612c89692b2904639240f7d5
SushiRoll53 <brfigueroa@miners.utep.edu>
2019-05-16 10:56:23 -0600

    Integrating proxy and gui

--- Proxy.py
+++ Proxy.py
@@ -4,43 +4,49 @@ from netfilterqueue import NetfilterQueue
 from HookManager.Hook import Hook
 from HookManager.HookCollection import HookCollection
 from rules import rules as rules
-import sys
-from io import BytesIO
+from threading import Thread
+from time import sleep
 
-def modify(packet):
-    if rules.captureFilterStatus:
-        testHook = Hook("Test",True,"Neat!",2,"testHook.py")
-        hooks = []
-        hc = HookCollection("testCollection",0,True,"Test hook coll.",hooks)
-        hc.addHook(testHook)
-        newpkt = hc.executeHookSequence(packet)
+class Proxy(Thread):
+	proxyStatus = False;
+	def __init__(self):
+		print("Proxy")
+		self.proxyStatus = False;
+		self.rule = rules()
+		super().__init__()
 
+	def modify(self, packet):
+		if rules.captureFilterStatus:
+			testHook = Hook("Test",True,"Neat!",2,"testHook.py")
+			hooks = []
+			hc = HookCollection("testCollection",0,True,"Test hook coll.",hooks)
+			hc.addHook(testHook)
+			newpkt = hc.executeHookSequence(packet)
 
-    pkt = IP(packet.get_payload()) #converts the raw packet to a scapy compatible string
+		pkt = IP(packet.get_payload()) #converts the raw packet to a scapy compatible string
 
-    #pktAsB = bytes(pkt)
-    
-    #print(pktAsB)
+		pkt.summary()
 
-    a = pkt.sprintf("%.time% %-15s,IP.src% -> %-15s,IP.dst% %IP.chksum% "
-                    "%03xr,IP.proto% %r,TCP.flags%")
-
-    for layer in pkt:
-        print(layer.dst)
-
-    packet.accept() #accept the packet
-
+		packet.accept() #accept the packet
 
 
-rule = rules()
-#rule.enableFilter() To enable filter
-rule.set()
-nfqueue = NetfilterQueue()
-#1 is the iptabels rule queue number, modify is the callback function
-nfqueue.bind(1, modify)
-try:
-    print ("[*] waiting for data")
-    nfqueue.run()
-except KeyboardInterrupt:
-    rule.flush()
-    pass
+	def run(self):
+		print("running")
+		#while(self.proxyStatus):
+		#rule.enableFilter() To enable filter
+		nfqueue = NetfilterQueue()
+		nfqueue.bind(1, self.modify)
+		#try:
+		print ("[*] waiting for data")
+		nfqueue.run()
+		#except KeyboardInterrupt:
+
+			#   pass
+
+	def turnOn(self):
+		self.rule.set()
+		self.proxyStatus = True
+
+	def turnOff(self):
+		self.rule.flush()
+		self.proxyStatus = False
